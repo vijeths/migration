e0129d1cd791b024182645cf7e3d4dc29bc183a2
vijeths <82367045+vijeths@users.noreply.github.com>
2022-01-20 17:10:13 +0530

    Update migrate_repo.sh

--- migrate_repo.sh
+++ migrate_repo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ ! -f $1 ]]; then echo "$1 file not found"; exit 99; fi
-
-read -p 'GitHub Username (not email): ' GH_USERNAME
-read -sp 'GitHub Password: ' GH_PASSWORD
-read -p 'BitBucket Username (not email): ' BB_USERNAME
-read -sp 'BitBucket Password: ' BB_PASSWORD
+:"
+#read -p 'GitHub Username (not email): ' GH_USERNAME
+#read -sp 'GitHub Password: ' GH_PASSWORD
+#read -p 'BitBucket Username (not email): ' BB_USERNAME
+#read -sp 'BitBucket Password: ' BB_PASSWORD
 
 (cat "$1" ; echo) | tail -n +2 | tr -d '\r' | while IFS=, read -r bb_repo bb_org gh_repo gh_org description archive private
 do
@@ -15,7 +15,7 @@ do
   #next_url="https://api.bitbucket.org/2.0/repositories?role=member"
   #test=$(curl -s --user $BB_USERNAME:$BB_PASSWORD "$next_url")
   #echo "$test"
-  git clone --mirror https://$BB_USERNAME:$BB_PASSWORD@bitbucket.org/$bb_org/$bb_repo.git
+  git clone --mirror https://${{ secrets.BB_USERNAME }}:${ secrets.BB_PASSWORD }}@bitbucket.org/$bb_org/$bb_repo.git
   cd $bb_repo.git
   echo
 
@@ -25,11 +25,11 @@ do
     echo "PUBLIC!"
     PRIVATE_FLAG="false"
   fi
-  curl -u $GH_USERNAME:$GH_PASSWORD https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
+  curl -u ${{ secrets.GH_USERNAME }}:${{ secrets.GH_PASSWORD }} https://api.github.com/orgs/$gh_org/repos -d "{\"name\": \"$gh_repo\", \"private\": $PRIVATE_FLAG, \"description\": \"$description\"}"
   echo
 
   echo "=== pushing $gh_org/$gh_repo to GitHub..."
-  git push --mirror https://$GH_USERNAME:$GH_PASSWORD@github.com/$gh_org/$gh_repo.git
+  git push --mirror https://${{ secrets.GH_USERNAME }}:${{ secrets.GH_PASSWORD }}@github.com/$gh_org/$gh_repo.git
   echo
 
   if [ "$archive" = "yes" ]; then
